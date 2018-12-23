6a9e4c7a0e31e48f6395330f24c7c88df942ba0b
vipstar555 <ygtug89580@yahoo.co.jp>
2018-12-24 04:06:59 +0900

    WIP PostgreSQL接続作成

---
+++ KabutanLib/Kabutan.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using System.Net;
+using AngleSharp.Parser.Html;
+
+namespace KabutanLib
+{
+    static public class KaiziClient
+    {
+        //{0}:何ページ目
+        const string KaiziURL = @"https://kabutan.jp/disclosures/?kubun=&page={0}";
+        
+        static public IEnumerable<KaiziItem> GetKaiziItems(int page)
+        {
+            var wc = new System.Net.WebClient();
+            var stream = wc.OpenRead(string.Format(KaiziURL, page));
+            var parser = new HtmlParser();
+            var doc = parser.Parse(stream);
+            var trs = doc.QuerySelectorAll("#main > div.disclosure_box > table > tbody > tr");
+            foreach(var tr in trs)
+            {
+                //[0]:Code　[1]:Market
+                var tacs = tr.QuerySelectorAll("td.tac");
+                //[0]:InfoType [1]:Title
+                var tals = tr.QuerySelectorAll("td.tal");
+                yield return new KaiziItem
+                {
+                    Code = (int.TryParse(tacs[0].TextContent, out var code)) ? code : -1,
+                    Name = tr.QuerySelector("th.tal").TextContent,
+                    Market = tacs[1].TextContent,
+                    InfoType = tals[0].TextContent,
+                    Title = tals[1].TextContent,
+                    DateTime = DateTime.TryParse(tr.QuerySelector("time").GetAttribute("datetime"), out var datetime) ? datetime : DateTime.MinValue,
+                    PDFURL = tals[1].QuerySelector("a").GetAttribute("href").ToString(),
+            };
+
+            }
+
+        }
+    }
+
+    public class KaiziItem
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Market { get; set; }
+        public string InfoType { get; set; }
+        public string Title { get; set; }
+        public DateTime DateTime { get; set; }
+        public string PDFURL { get; set; }
+    }
+}
---
+++ KabutanLib/KabutanPostgreSQL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;        // 参照を追加。
+using System.ComponentModel.DataAnnotations.Schema; // 参照を追加。
+using Npgsql;
+using System.IO;
+
+namespace KabutanLib
+{
+    public class KabutanPostgreSQLContext : DbContext
+    {
+        const string configFilePath = "../../../metadata/test.txt";
+
+        public string DefaultSchema { get; private set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            => modelBuilder.HasDefaultSchema(DefaultSchema);
+
+        public KabutanPostgreSQLContext()
+            : base(Con(), true)
+        {
+            DefaultSchema = "";
+        }
+        static public NpgsqlConnection Con()
+        {
+            using (var sr = new StreamReader(configFilePath))
+            {
+                var sb = new StringBuilder();
+                sb.Append($"Server={sr.ReadLine()};")
+                .Append($"Port={sr.ReadLine()};")
+                .Append($"Database={sr.ReadLine()};")
+                .Append($"User Id={sr.ReadLine()};")
+                .Append($"Password={sr.ReadLine()};");
+                return new NpgsqlConnection(sb.ToString());
+            }                            
+        }
+        public DbSet<Table> Tests { get; set; }
+    }
+    public class KabutanPostgreSQL
+    {
+        static public void TestSQL()
+        {
+            var a = new KabutanPostgreSQLContext();
+            {
+                var sds = a.Tests.ToList();
+                return;
+            }
+        }
+    }
+    [Table("Test")]
+    public class Table
+    {
+        [Key]
+        [Column("id")]
+        public int ID { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
