e3f8163e1ea2ac8a2f88000d6fae8f7b14d23f2f
vipstar555 <ygtug89580@yahoo.co.jp>
2018-12-29 00:48:48 +0900

    ツイッター通知部分の作成（テスト段階）

--- KabuInformationForTwitter/Program.cs
+++ KabuInformationForTwitter/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using KabutanLib;
+using System.Threading;
 
 namespace KabuInformationForTwitter
 {
@@ -11,13 +12,45 @@ namespace KabuInformationForTwitter
     {
         static void Main(string[] args)
         {
-            var kabutans = KaiziClient.GetKaiziItems(1);
-            var con = new KabutanPostgreSQLContext();
-            foreach(var kabutan in kabutans)
+            string latestPdfURL = "";
+            var financeConnect = new CodeListDbContext.FinanceConnect();
+            var con = financeConnect.GetContext();
+            IEnumerable<KaiziItem> kaiziItems;
+            var tweetToken = Twitter.GetToken();
+            while (true)
             {
-                con.KaiziItems.Add(kabutan);
+                //とりあえず15個分で→初期値どこかで決定する（DB化実用始めたらそこまで読み取るとかも）
+                kaiziItems = KaiziClient.GetKaiziItems(1);
+                
+                if(latestPdfURL == "" || kaiziItems.Count() > 0)
+                {
+                    latestPdfURL = kaiziItems.FirstOrDefault().PDFURL;
+                }
+                
+                foreach(var kaiziItem in kaiziItems)
+                {
+                    if(kaiziItem.PDFURL == latestPdfURL )
+                    {
+                        break;
+                    }
+                    //最新の時価総額
+                    var latestMarketCapitalization = con.TradeIndexs.Where(x => x.code == kaiziItem.Code).OrderByDescending(x => x.date).FirstOrDefault().marketCapitalization;
+                    //ツイート内容
+                    var tweetText = string.Format($"{kaiziItem.Code} {kaiziItem.Name} 時価{latestMarketCapitalization/100000000:0.00}億\r\n{kaiziItem.Title}");
+                    //140文字に収める
+                    if(tweetText.Length > 140)
+                    {
+                        tweetText = tweetText.Substring(0, 139);
+                    }                    
+                    tweetToken.Statuses.Update(tweetText + "\r\n" + kaiziItem.PDFURL);
+                    Console.WriteLine($"{DateTime.Now} Tweet" + tweetText + "\r\n" + kaiziItem.PDFURL);
+                }
+                //最新通知済開示内容を記録
+                latestPdfURL = kaiziItems.FirstOrDefault().PDFURL;
+                Thread.Sleep(10000);
             }
-            con.SaveChanges();
+
+            
         }
     }
 }
---
+++ KabuInformationForTwitter/Twitter/Twitter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreTweet;
+using System.IO;
+
+namespace KabuInformationForTwitter
+{
+    public class Twitter
+    {
+        const int limit = 140;
+        const string tokenFilePath = "../../../metadata/twetterAPI.txt";
+        static public Tokens GetToken()
+        {
+            using (var sr = new StreamReader(tokenFilePath))
+            {
+                return CoreTweet.Tokens.Create
+                        (
+                            sr.ReadLine(),
+                            sr.ReadLine(),
+                            sr.ReadLine(),
+                            sr.ReadLine()
+                        );
+            }
+        }
+    }
+}
